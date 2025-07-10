96a4fd55b2d0346c8ec22977947d20d6987d92ff
Eranga Madushan <enfera.official@gmail.com>
2025-07-10 15:56:34 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/Enfera.github.io/',
   plugins: [react(), tailwindcss()],
 });
 //this is first comment from Ernaga for test 
